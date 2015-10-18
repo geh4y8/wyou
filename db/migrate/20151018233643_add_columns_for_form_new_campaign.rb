46829a22045c9eed852b03ed7d2a314797faec2a
geh4y8 <guy.halperin88@gmail.com>
2015-10-18 16:44:44 -0700

    add columns for new campaign form to campaigns table

---
+++ db/migrate/20151018233643_add_columns_for_form_new_campaign.rb
@@ -0,0 +1,12 @@
+class AddColumnsForFormNewCampaign < ActiveRecord::Migration
+  def change
+    add_column :campaigns, :patient_name, :string
+    add_column :campaigns, :patient_email, :string
+    add_column :campaigns, :patient_phone, :string
+    add_column :campsigns, :relationship, :string
+    add_column :campaigns, :owner_name, :string
+    add_column :campaigns, :self_purchase, :boolean
+    add_column :campaigns, :owner_email, :string
+    add_column :campaigns, :inform_patient_date, :date
+  end
+end
