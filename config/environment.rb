39b0f29713ead91f6c1cff74203fc6493187ef16
Guy Halperin <geh4y8@gmail.com>
2016-02-22 17:20:44 -0800

    add env vars

--- config/environment.rb
+++ config/environment.rb
@@ -5,6 +5,5 @@ require File.expand_path('../application', __FILE__)
 # Load heroku vars from local file
 heroku_env = File.join(Rails.root, 'config', 'heroku_env.rb')
 load(heroku_env) if File.exists?(heroku_env)
-
 # Initialize the Rails application.
 Rails.application.initialize!
