26dcdcf0c6d6ded902c677ea8515477a37d2fcdd
Guy Halperin <geh4y8@gmail.com>
2016-02-27 13:56:44 -0800

    edit mailer

--- app/mailers/invite_mailer.rb
+++ app/mailers/invite_mailer.rb
@@ -1,11 +1,10 @@
 class InviteMailer < ApplicationMailer
-
+  default from: 'notifications@wyou.co'
   def invite_contributor_email(invite)
     @invite = invite
     @campaign = @invite.campaign
     @url = "wyoutestdeploy1.herokuapp.com/users/sign_up"
     mail(
-      from: @campaign.owner_email,
       to: @invite.contributor_email,
       subject: "[w/you] You have been invited to contribute!"
     )
