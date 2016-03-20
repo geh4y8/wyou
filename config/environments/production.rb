312a91d32c260e4ed21e4d1dd9c48a4af1080f27
Guy Halperin <geh4y8@gmail.com>
2016-03-20 15:36:24 -0700

    prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,7 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   config.action_controller.asset_host = 'dxrn0537r7wh8.cloudfront.net'
 
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
@@ -82,7 +83,7 @@ Rails.application.configure do
   }
   # ActionMailer Config
   config.action_mailer.default_url_options = { :host => 'dxrn0537r7wh8.cloudfront.net' }
-  config.action_mailer.asset_host = 'dxrn0537r7wh8.cloudfront.net'
+  config.action_mailer.asset_host = 'http://dxrn0537r7wh8.cloudfront.net'
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
