6a9ec71f0452858485842fda07b73c2e7abc9c14
Guy Halperin <geh4y8@gmail.com>
2016-03-20 15:25:53 -0700

    add production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,6 +82,7 @@ Rails.application.configure do
   }
   # ActionMailer Config
   config.action_mailer.default_url_options = { :host => 'dxrn0537r7wh8.cloudfront.net' }
+  config.action_mailer.asset_host = 'dxrn0537r7wh8.cloudfront.net'
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
