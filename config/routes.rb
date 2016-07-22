9ed785c5abfeed1822c71306965e574da0083b97
Guy Halperin <geh4y8@gmail.com>
2016-07-21 19:16:08 -0700

    add possible campaign route

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,7 @@ Rails.application.routes.draw do
   get '/admin/campaigns/donations' => 'admin#donations'
   get '/remove_product_category' => 'categories#remove_product'
   get '/connect/confirm' => 'stripe#confirm'
+  get '/possible_campaigns' => 'campaigns#possible_campaigns2'
 
 
 
