ddfb05c5af453e9d533e80872dc36d2417efd134
Guy Halperin <geh4y8@gmail.com>
2016-08-22 19:27:30 -0700

    send different url if user has not yet been created on donation emails

--- app/mailers/donation_mailer.rb
+++ app/mailers/donation_mailer.rb
@@ -15,7 +15,11 @@ class DonationMailer < ApplicationMailer
     @campaign = campaign
     @user = user
     @donation_amount = donation_amount
-    @url = "wyou.co/campaigns/#{@campaign.id}"
+    if User.find_by(email: @campaign.patient_email)
+      @url = "wyou.co/campaigns/#{@campaign.id}"
+    else
+      @url = "wyou.co/users/sign_up?campaign_code=#{@campaign.campaign_code}&name=#{@campaign.patient_name}&email=#{@campaign.patient_email}"
+    end
     mail(
       to: @campaign.patient_email,
       subject: "[w/you] Donation Received!")
